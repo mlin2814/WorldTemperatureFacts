6aebcd0424b13ce74f66fe6f69ada65553d34c56
Matthew Lin <mlin2814@gmail.com>
2022-02-27 11:54:58 -0500

    Have the user location write and read function working from teammate's microservice, need to do the random location one now

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const constants = {
     openWeatherMap: {
         BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
-        SECRET_KEY: ""
+        SECRET_KEY: "b4c841e97d9659b5f8d1db63513a1ea7"
     }
 }
 
--- public/js/app.js
+++ public/js/app.js
@@ -1,69 +1,38 @@
 var fetchWeather = "/weather";
-// const fs = require('fs');
 
 const weatherForm = document.querySelector('form');
+
 const search = document.querySelector('input');
-
-// const weatherIcon = document.querySelector('.weatherIcon i');
-
-// const weatherCondition = document.querySelector('.weatherCondition');
 
 const tempElement = document.querySelector('.temperature span');
 
 const locationElement = document.querySelector('.place');
 
-// const testweatherIcon = document.querySelector('.testweatherIcon i');
-// const testweatherCondition = document.querySelector('.testweatherCondition');
-
 const testtempElement = document.querySelector('.testtemperature span');
 
 const testlocationElement = document.querySelector('.testplace');
 
-// const dateElement = document.querySelector('.date');
-
-// const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
-// dateElement.textContent = new Date().getDate() + ", " + monthNames[new Date().getMonth()].substring(0, 3);
 
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault();
     locationElement.textContent = "Loading...";
     tempElement.textContent = "";
-    // weatherCondition.textContent = "";
     const locationApi = fetchWeather + "?address=" + search.value + "&units=imperial";
     fetch(locationApi).then(response => {
         response.json().then(data => {
             if(data.error) {
                 locationElement.textContent = data.error;
                 tempElement.textContent = "";
-                // weatherCondition.textContent = "";
             } else {
-                // console.log()
-                // if(data.description === "rain" || data.description === "fog") {
-                //     weatherIcon.className = "wi wi-day-" + data.description
-                // } else {
-                //     weatherIcon.className = "wi wi-day-cloudy"
-                // }
                 locationElement.textContent = "Your Location: " + data.cityName;
                 // tempElement.textContent = (data.temperature - 273.5).toFixed(2) + String.fromCharCode(176);
                 tempElement.textContent = (data.temperature * 1.8 - 459.67).toFixed(2) + String.fromCharCode(176) + "F";
                 userConvertTemp = (data.temperature * 1.8 - 459.67).toFixed(2);
                 console.log(userConvertTemp);
-                // fs.writeFile('Location1.txt', userConvertTemp, (err) => {
-                //     if (err) {
-                //         throw err;
-                //     }
-                // })
                 var myFile = new File([userConvertTemp], "Location1.txt", {type: "text/plain;charset=utf-8"});
                 saveAs(myFile);
-
-                // fs.readFile('Location1.txt', function (err, data) {
-                //     if (err) {
-                //        return console.error(err);
-                //     }
-                //     console.log("Asynchronous read: " + data.toString());
-                // })
+                
                 document.getElementById('fileInput').addEventListener('change', function selectedFileChanged() {
                     // console.log(this.files); // will contain information about the file that was selected.
                     if (this.files.length === 0) {
@@ -75,12 +44,12 @@ weatherForm.addEventListener('submit', (event) => {
                       reader.onload = function fileReadCompleted() {
                         // when the reader is done, the content is in reader.result.
                         console.log(reader.result);
+                        var userTempC = reader.result;
+                        tempElement.textContent = (data.temperature * 1.8 - 459.67).toFixed(2) + String.fromCharCode(176) + "F / " + userTempC +"C";
                       };
                       reader.readAsText(this.files[0]);
                 });
 
-
-                // weatherCondition.textContent = data.description.toUpperCase();
             }
         }) 
     }),
